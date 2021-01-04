5831f353bde82110dbf55e175bf1f7fd7a34507b
CharleyDvs <ing.carlosjaime@outlook.com>
2021-01-03 18:39:23 -0600

    Added logo

--- src/App.js
+++ src/App.js
@@ -2,9 +2,11 @@ import React from 'react'
 import { GlobalStyle } from './globalStyles'
 import { ListOfCategories } from './components/ListOfCategories'
 import { ListOfPhotoCards } from './components/ListOfPhotocard'
+import Logo from './components/Logo'
 
 export const App = () => (
   <>
+    <Logo />
     <GlobalStyle />
     <ListOfCategories />
     <ListOfPhotoCards />
