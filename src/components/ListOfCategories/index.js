c7d415d6dbcaa446de09dc0bb58c02fde2a96558
CharleyDvs <ing.carlosjaime@outlook.com>
2021-01-03 21:06:27 -0600

    Added a fixed categories list

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Category } from '../Category'
 import { List, Item } from './styles'
-import { categories } from '../../../api/db.json'
 
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([])
+  const [showFixed, setShowFixed] = useState(false)
+  useEffect(() => {
+    try {
+      window
+        .fetch('https://petgram-server-cjy-7z0oguq47.vercel.app/categories')
+        .then((response) => response.json())
+        .then((data) => setCategories(data))
+    } catch (err) {
+      console.log(err)
+    }
+  }, [])
+
+  useEffect(() => {
+    const onScroll = (e) => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener('scroll', onScroll)
+    return () => {
+      document.removeEventListener('scroll', onScroll)
+    }
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {categories.map((category) => (
         <Item key={category}>
           <Category {...category} />
         </Item>
       ))}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
