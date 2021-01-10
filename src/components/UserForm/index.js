8b761bbb172561baaa781ec087a4a757b6349217
CharleyDvs <ing.carlosjaime@outlook.com>
2021-01-10 10:39:16 -0600

    Added mutation to register user

--- src/components/UserForm/index.js
+++ src/components/UserForm/index.js
@@ -1,15 +1,35 @@
 import React from 'react'
 import { useInputValue } from '../../hooks/useInputForm'
+import { Title, Form, Input, Button } from './styles'
 
-export const UserForm = ({ onSubmit }) => {
+export const UserForm = ({
+  disabled,
+  error,
+  onSubmit,
+  title = 'Regístrate'
+}) => {
   const email = useInputValue('')
   const password = useInputValue('')
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    onSubmit({ email: email.value, password: password.value })
+  }
+
   return (
-    <form onSubmit={onSubmit}>
-      <input type="email" placeholder="Email" {...email} />
-      <input type="password" placeholder="Password" {...password} />
-      <button>Iniciar sesión</button>
-    </form>
+    <Form disabled={disabled} onSubmit={handleSubmit}>
+      <Title>{title}</Title>
+      <Input disabled={disabled} type="email" placeholder="Email" {...email} />
+      <Input
+        disabled={disabled}
+        type="password"
+        placeholder="Password"
+        {...password}
+      />
+      <Button disabled={disabled} type="submit">
+        {title}
+      </Button>
+      {error && <p>{error}</p>}
+    </Form>
   )
 }
