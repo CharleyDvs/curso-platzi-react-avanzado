d9bafee1d6d25f471af6b94b37abcbe7ba0963ed
CharleyDvs <ing.carlosjaime@outlook.com>
2021-01-10 10:57:23 -0600

    Added Log in mutation

--- src/pages/NotRegisteredUser.js
+++ src/pages/NotRegisteredUser.js
@@ -2,29 +2,51 @@ import React from 'react'
 import Context from '../Context'
 import { UserForm } from '../components/UserForm'
 import { RegisterMutation } from '../container/RegisterMutation'
+import { LoginMutation } from '../container/LoginMutation'
 
 export const NotRegisteredUser = () => (
   <Context.Consumer>
     {({ activateAuth }) => {
       return (
-        <RegisterMutation>
-          {(register, { data, loading, error }) => {
-            const onSubmit = ({ email, password }) => {
-              const input = { email, password }
-              const variables = { input }
-              register({ variables }).then(activateAuth)
-            }
-            const errorMsg =
-              error && 'El usuario ya existe o hay algún problema'
-            return (
-              <UserForm
-                disabled={loading}
-                error={errorMsg}
-                onSubmit={onSubmit}
-              />
-            )
-          }}
-        </RegisterMutation>
+        <>
+          <RegisterMutation>
+            {(register, { data, loading, error }) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password }
+                const variables = { input }
+                register({ variables }).then(activateAuth)
+              }
+              const errorMsg =
+                error && 'El usuario ya existe o hay algún problema'
+              return (
+                <UserForm
+                  disabled={loading}
+                  error={errorMsg}
+                  onSubmit={onSubmit}
+                />
+              )
+            }}
+          </RegisterMutation>
+          <LoginMutation>
+            {(login, { data, loading, error }) => {
+              const onSubmit = ({ email, password }) => {
+                const input = { email, password }
+                const variables = { input }
+                login({ variables }).then(activateAuth)
+              }
+              const errorMsg =
+                error && 'El password es incorrecto o el usuario no existe'
+              return (
+                <UserForm
+                  disabled={loading}
+                  error={errorMsg}
+                  onSubmit={onSubmit}
+                  title="Log in"
+                />
+              )
+            }}
+          </LoginMutation>
+        </>
       )
     }}
   </Context.Consumer>
