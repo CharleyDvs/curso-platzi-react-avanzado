7d2346a69881be18c1b6604736c3c03bd1558c53
CharleyDvs <ing.carlosjaime@outlook.com>
2021-01-10 19:22:16 -0600

    Added a 404 error page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { GlobalStyle } from './styles/globalStyles'
 import Logo from './components/Logo'
 
@@ -7,36 +7,30 @@ import { Detail } from './pages/Detail'
 import { NavBar } from './components/Navbar'
 import { User } from './pages/User'
 import { Favs } from './pages/Favs'
+import { NotFound } from './pages/NotFound'
 import { NotRegisteredUser } from './pages/NotRegisteredUser'
 
-import { Router } from '@reach/router'
-import Context from './Context'
+import { Redirect, Router } from '@reach/router'
+import { Context } from './Context'
 
 export const App = () => {
+  const { isAuth } = useContext(Context)
   return (
     <>
       <GlobalStyle />
       <Logo />
       <Router>
+        <NotFound default />
         <Home path="/" />
         <Home path="/pet/:id" />
         <Detail path="/detail/:detailId" />
+        {!isAuth && <NotRegisteredUser path="/login" />}
+        {!isAuth && <Redirect from="/favs" to="/login" />}
+        {!isAuth && <Redirect from="/user" to="/login" />}
+        {isAuth && <Redirect from="/login" to="/" />}
+        <Favs path="/favs" />
+        <User path="/user" />
       </Router>
-      <Context.Consumer>
-        {({ isAuth }) =>
-          isAuth ? (
-            <Router>
-              <Favs path="/favs" />
-              <User path="/user" />
-            </Router>
-          ) : (
-            <Router>
-              <NotRegisteredUser path="/favs" />
-              <NotRegisteredUser path="/user" />
-            </Router>
-          )
-        }
-      </Context.Consumer>
       <NavBar />
     </>
   )
