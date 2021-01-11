d394fc57b4e32b3aa359e05d21cce1a7cc0f6028
CharleyDvs <ing.carlosjaime@outlook.com>
2021-01-10 20:03:31 -0600

    Added Helmet and a layout component

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { PhotocardWithQuery } from '../container/PhotoCardWithQuery'
+import { Layout } from '../components/Layout'
 
 export const Detail = ({ detailId }) => {
-  return <PhotocardWithQuery id={detailId} />
+  return (
+    <Layout title={`Fotografía ${detailId}`}>
+      <PhotocardWithQuery id={detailId} />
+    </Layout>
+  )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Layout } from '../components/Layout'
 
 import { ListOfCategories } from '../components/ListOfCategories'
 import { ListOfPhotoCards } from '../container/ListOfPhotoCards'
 
 export const Home = ({ id }) => {
   return (
-    <>
+    <Layout
+      title="Tu app de mascotas"
+      subtitle="Encuantra las mejores fotos de animales con Petgram"
+    >
       <ListOfCategories />
       <ListOfPhotoCards categoryId={id} />
-    </>
+    </Layout>
   )
 }
